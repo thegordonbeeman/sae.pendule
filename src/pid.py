f72b54c7a3bc9c3e8279b156ab045f456138fc77
Thomas Laburthe <thomas.laburthe@gmail.com>
2025-02-12 09:47:57 +0100

    PID & PIDMonitor

---
+++ src/pid.py
@@ -0,0 +1,96 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+import math
+
+# SP: SetPoint
+# PV: ProcessValue
+# Err: SP - PV
+# P: Err * Kp
+# I: somme(Err) * Ki
+# D: dérivée(Err) * Kd
+
+class PID():
+	def __init__(self, Kp, Ki, Kd):
+		self.Kp = Kp
+		self.Ki = Ki
+		self.Kd = Kd
+
+		self.SP, self.PV = 0, 0
+		self.Err, self.last_Err, self.sum_Err = 0, 0, 0
+		self.Output = 0
+	
+	def get_SP(self):
+		return self.SP
+
+	def set_SP(self, SP):
+		self.SP = SP
+	
+	def get_PV(self):
+		return self.PV
+	
+	def set_PV(self, PV):
+		self.PV = PV
+
+	def update(self, ts):
+		self.Err = self.SP - self.PV
+		self.sum_Err += self.Err
+
+		P = self.Err * self.Kp
+		I = self.sum_Err * self.Ki
+		D = (self.last_Err - self.Err) * self.Kd / ts
+
+		self.Output = P + I + D
+
+		self.last_Err = self.Err
+
+		return self.Output
+
+class PIDMonitor():
+	def __init__(self, pid, sample_count):
+		self.pid = pid
+		self.sample_count = sample_count
+
+		self.SP = np.zeros((sample_count))
+		self.PV = np.zeros((sample_count))
+		self.Err = np.zeros((sample_count))
+		self.Output = np.zeros((sample_count))
+
+		self.index = 0
+	
+	def update(self):
+		if (self.index > self.sample_count - 1):
+			return
+		
+		self.SP[self.index] = self.pid.SP
+		self.PV[self.index] = self.pid.PV
+		self.Err[self.index] = self.pid.Err
+		self.Output[self.index] = self.pid.Output
+
+		self.index += 1
+
+if __name__ == "__main__":
+	ts = 0.005
+	sample_count = 1000
+
+	pid = PID(2.0, 0.1, 0.0)
+	pidm = PIDMonitor(pid, sample_count)
+
+	PV = 0
+
+	for i in range(sample_count):
+		pid.set_SP(abs(math.sin(i / 100)) * 20)
+		pid.set_PV(PV)
+
+		pid.update(ts)
+		pidm.update()
+
+		PV += pid.Output * 0.03
+
+	xs = np.linspace(0, sample_count * ts, sample_count)
+	plt.plot(xs, pidm.SP)
+	plt.plot(xs, pidm.PV)
+	plt.plot(xs, pidm.Err)
+	plt.plot(xs, pidm.Output)
+	plt.legend(['SP', 'PV', 'Err', 'Output'])
+	plt.show()
